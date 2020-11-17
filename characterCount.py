395e2d97079f8024e19133670f976207e3c9314d
Olb2 <olgaborysovska@gmail.com>
2020-11-17 12:28:21 -0800

    adds tic-yac-toe game (not comleted)

--- characterCount.py
+++ characterCount.py
@@ -1,4 +1,6 @@
 #calculate the amount of charachters in the string 
+import pprint 
+
 message = 'it was a bright cold day in April, and the clocks were striking thirteen.'
 
 #with setdefault() method
@@ -6,7 +8,7 @@ count = {}
 for character in message:
     count.setdefault(character , 0)
     count[character] = count[character] +1 
-print(count)
+pprint.pprint(count)
 
 #another version 
 c = {} 
