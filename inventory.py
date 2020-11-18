642f0f430a81ba97dc1eb4d77e88a3edc62808d6
Olb2 <olgaborysovska@gmail.com>
2020-11-18 11:25:22 -0800

    adds problem/solution to display inventory(dictionary)

---
+++ inventory.py
@@ -0,0 +1,42 @@
+#write a function named displayInventory() 
+# that would take any possible 'inventory' and dsipaly like the folowing: 
+# Inventory:
+# 12 arrows
+# 42 gold coins
+# 1 rope
+# 6 torches
+# 1 dagger
+# Total number of items: 62
+
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+nums = {'Math': 0, 'English': 0, 'Science': 0}
+fruit = {"apple": 430, "banana": 312, "orange": 525, "pear": 217}
+
+def displayInventory(inventory):
+    print('Inventory:')
+    total = 0 
+
+    word_ending = ['s', 'sh', 'ch', 'x', 'z'] # use 'es' to make a plural all oter use 's'
+    
+    for key, value in inventory.items():
+        
+        if value <= 1: 
+            print(value , key)
+        elif value != 1:
+            if key[-1] in word_ending or key[-2:] in word_ending and value > 1:
+                print(value, key + 'es')
+            else:
+                print(value, key + 's')
+
+        total += value 
+
+    print('Total number of items: ', total) 
+
+    return '' 
+
+print(displayInventory(stuff))
+print(displayInventory(nums))
+print(displayInventory(fruit))
+    
+
+
