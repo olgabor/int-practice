582cbfb336ae647609a05f2e1c41ff732ae82124
Olb2 <olgaborysovska@gmail.com>
2020-11-30 10:56:31 -0800

    adds two sum problem solution

--- two_sum.py
+++ two_sum.py
@@ -5,18 +5,20 @@
 #Example 1:
 
 nums = [2,7,11,15]
-nums1 = [11,15, 2,7]
-nums2 = [2, 11,15,7]
-target = 9
+nums1 = [3,2,4] 
+nums2 = [3, 3]
 #Output: [0,1]
 #Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 def two_sum(nums, target): 
-    output = []
     for i in range(len(nums)):
-        if nums[i] < target: 
+        for j in range(i+1 , len(nums)):
+            # print(i, j )
+            if nums[i] + nums[j] == target:
+                return i , j 
 
     
-print(two_sum(nums, target))
-print(two_sum(nums1, target))
-print(two_sum(nums2, target))
+print(two_sum(nums, 9))
+print(two_sum(nums1, 6))
+print(two_sum(nums2, 6))
+
