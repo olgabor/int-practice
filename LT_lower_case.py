287b46e24e44fc640ee43d3f856e9502c1a915b8
Olb2 <olgaborysovska@gmail.com>
2020-12-01 15:12:06 -0800

    adds string to lowercase with ASCII codes(leetcode) solution

---
+++ LT_lower_case.py
@@ -0,0 +1,25 @@
+
+#Implement function ToLowerCase() that has a string parameter str, 
+# and returns the same string in lowercase.
+
+# ord() returns ASCII code 
+# chr() returns 
+
+
+def toLowerCase(inp):
+    if len(inp) == 0:
+        return 'string is empty'
+    else: 
+        for letter in inp: 
+            if ord(letter) <= 90 and ord(letter) >= 65: #uppercase letters in ASCII are higher than 90 and lower than 65 
+                inp = inp.replace(letter, chr(ord(letter) + 32)) 
+            
+    return inp 
+    
+print(toLowerCase("Hello"))
+print(toLowerCase("here"))
+print(toLowerCase("Apple"))
+print(toLowerCase("LOVELY"))
+print(toLowerCase("al&phaBET"))
+print(toLowerCase("PiTAs"))
+print(toLowerCase("PiTAs24<<<<"))
