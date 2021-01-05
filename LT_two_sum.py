a80703f9882b04b2539a486f4441716e9597c778
Olb2 <olgaborysovska@gmail.com>
2021-01-04 19:04:27 -0800

    adds hash map solution for two_sum problem

--- LT_two_sum.py
+++ LT_two_sum.py
@@ -4,8 +4,8 @@
 #You can return the answer in any order.
 #Example 1:
 
-nums = [2,7,11,15]
-nums1 = [3,2,4] 
+nums = [2, 7, 11, 15]
+nums1 = [3, 2, 4] 
 nums2 = [3, 3]
 #Output: [0,1]
 #Output: Because nums[0] + nums[1] == 9, we return [0, 1].
@@ -17,9 +17,24 @@ def two_sum(nums, target):
                 return i , j 
        
     return 'no numbers add up to target'
-
     
-print(two_sum(nums, 9))
-print(two_sum(nums1, 6))
-print(two_sum(nums2, 6))
+# print(two_sum(nums, 9))
+# print(two_sum(nums1, 6))
+# print(two_sum(nums2, 6))
 
+#hash maps 
+def twoSumHashmap(nums, target): 
+
+    m = {}
+    for i in range(len(nums)):
+        
+        diff = target - nums[i]  
+
+        if diff in m:
+            return m[diff], i
+        else: 
+            m[nums[i]] = i  
+ 
+print(twoSumHashmap(nums, 9))
+print(twoSumHashmap(nums1, 6))
+print(twoSumHashmap(nums2, 6))
