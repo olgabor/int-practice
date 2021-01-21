52ea2daca28b7fd0c92e74d3d6ec1b690a68a9d6
Olb2 <olgaborysovska@gmail.com>
2021-01-21 10:41:42 -0800

    adds valid palindrome problem

--- algorithms_questions/remove_element.py
+++ algorithms_questions/remove_element.py
@@ -1,7 +1,5 @@
 # Given an array nums and a value val, remove all instances of that value in-place and return the new length.
-
 # Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-
 # The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
 
@@ -12,14 +10,20 @@ def removeElement(nums, value):
     while i <= j :
 
         if nums[i] == value:
-            
             del nums[i]
             j = len(nums) - 1 
         else: 
             i += 1 
     
-    return len(nums), nums
-   
+    return len(nums)
+
+def removeElementSorted(nums, value):
+    for n in sorted(nums):
+        if n == value:
+            nums.remove(n)
+        
+    return len(nums)
 
 print(removeElement([3,2,2,3], 3))
 print(removeElement([0,1,2,2,3,0,4,2], 2))
+print(removeElementSorted([0,1,2,2,3,0,4,2], 2))
---
+++ algorithms_questions/valid_palindrome.py
@@ -0,0 +1,44 @@
+# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+# Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+def validPalindrome(str):
+
+    start, end = 0, len(str) -1 
+
+    while start <= end :
+
+        while str[start].isalnum() != True: 
+            start += 1
+            
+        while str[end].isalnum() != True: 
+            end -= 1   
+           
+        if str[start].lower() != str[end].lower():
+            return False 
+
+        start += 1 
+        end -= 1
+
+    return True  
+    
+   
+print(validPalindrome("A man, a plan, a canal: Panama"))
+print(validPalindrome("race a car"))
+
+
+def validPalindromeRecursion(str):
+    
+    if len(str) == 0: 
+        return True 
+
+    if str[0] != str[-1]: 
+        return False 
+    
+    else:
+        return validPalindromeRecursion(str[1:-1])
+    
+    return True 
+
+print(validPalindromeRecursion("tacocat"))
+
