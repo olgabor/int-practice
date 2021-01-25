e2716a8c296b4e1085d4f64ddec26b96cbb3ff1a
Olb2 <olgaborysovska@gmail.com>
2021-01-25 12:18:31 -0800

    adds squares of sorted array

--- algorithms_questions/search_insert_position.py
+++ algorithms_questions/search_insert_position.py
@@ -2,13 +2,9 @@
 # If not, return the index where it would be if it were inserted in order.
 
 def insertPosition(nums, target):
-    # if number in list 
-         #return index
+
     pointer = 1 
     index = 0 
-    # index = nums.index(target) 
-    # if target == 0 and target not in nums: 
-    #     return 0 
     if target in nums: 
         return nums.index(target)
     
@@ -16,34 +12,19 @@ def insertPosition(nums, target):
 
         if target - pointer in nums:
             index =  nums.index(target - pointer) + 1 
-            # print('index line 17', index, pointer) 
             return index 
             
         if target + pointer  in nums: 
             index =  nums.index(target + pointer)
-            # print('index line 22', index) 
             return index 
         else: 
             pointer += 1 
-
-    # return index 
-    # print('pointer:', pointer)
-    # return  nums.index(target - pointer) + 1 
-
         
-    # print(index) 
-
-    # if number not in list: 
-        # find the value that is smaller by one and larger by one 
-        # if both exist 
-            #return smaller value index +1 
-
-        
-# print(insertPosition([1,2,5,6], 5 ))
-# print(insertPosition([1,2,5,6], 3 ))
-# print(insertPosition([1,3,5,6], 2))
+print(insertPosition([1,2,5,6], 5 ))
+print(insertPosition([1,2,5,6], 3 ))
+print(insertPosition([1,3,5,6], 2))
 print(insertPosition([1,3,5,6], 0))
-# print(insertPosition([2,3,4,8,10], 0))
+print(insertPosition([2,3,4,8,10], 0))
 print(insertPosition([2,3,4,7,8,9], 11))
 print(insertPosition([3,6,7,8,10], 5))
 print(insertPosition([2, 5], 1))
---
+++ algorithms_questions/squares_of_sorting_array.py
@@ -0,0 +1,20 @@
+# Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number 
+# sorted in non-decreasing order.
+
+# Input: nums = [-4,-1,0,3,10]
+# Output: [0,1,9,16,100]
+# Explanation: After squaring, the array becomes [16,1,0,9,100].
+# After sorting, it becomes [0,1,9,16,100].
+
+def sortedSquares(nums):
+    
+    output = [0] * len(nums)
+
+    for n in range(len(nums)): 
+        output[n] = nums[n] * nums[n]
+    
+    output.sort() 
+    return output 
+
+
+print(sortedSquares([-4,-1,0,3,10]))
