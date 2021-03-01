a755ed62d2cc5ede19aac2d76e3e7d4f731b854b
Olb2 <olgaborysovska@gmail.com>
2021-03-01 13:14:19 -0800

    adds deque folder with implementation class

---
+++ deque/deque.py
@@ -0,0 +1,32 @@
+class Deque: 
+
+    def __init__(self): 
+        self.items = []
+
+    def addFront(self, item):
+        self.items.insert(0, item)
+
+    def addRear(self, item):
+        self.items.append(item)
+    
+    def removeFront(self): 
+        return self.items.pop(0) 
+
+    def removeRear(self): 
+        return self.items.pop() 
+    
+    def size(self): 
+        return len(self.items) 
+    
+    def isEmpty(self): 
+        return self.items == []
+    
+
+
+d = Deque()
+print(d.size())
+print(d.isEmpty())
+print(d.addFront('Hello'))
+print(d.addRear('World'))
+
+print(d.removeFront() + ' ' + d.removeRear()) 
